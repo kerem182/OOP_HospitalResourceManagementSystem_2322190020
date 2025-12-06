f1921cd3317715cac82f06f26ba0957c43f580b1
Kerem Salih Erol <keremerol.2005@gmail.com>
2025-12-06 16:47:37 +0300

    Stage 1: Architecture Design and Initial Models

---
+++ app/models/init.py
@@ -0,0 +1,15 @@
+from .patient import Patient
+from .doctor import Doctor
+from .room import Room
+from .appointment import Appointment
+from .identifiable import Identifiable
+from .schedulable import Schedulable
+
+__all__ = [
+    "Patient",
+    "Doctor",
+    "Room",
+    "Appointment",
+    "Identifiable",
+    "Schedulable"
+]
